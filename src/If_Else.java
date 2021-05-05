b7ddf7900b4ad2a80651a1bdb0e5264569eeb51f
Dushyant Goyal <dushyantgoyal1999@gmail.com>
2021-05-05 16:52:56 +0530

    2nd Commit

--- src/If_Else.java
+++ src/If_Else.java
@@ -52,10 +52,10 @@ public class If_Else {
         if (N%2 == 1){
             System.out.println("Weird");
         }
-        else if (N%2 == 0 && N>=2 || N<=5){
+        else if (N%2 == 0 && N>=2 && N<=5){
             System.out.println("Not Weird");
         }
-        else if (N%2 == 0 && N>6 || N<20){
+        else if (N%2 == 0 && N>=6 && N<=20){
             System.out.println("Weird");
         }
         else if (N%2 == 0 && N>20){
