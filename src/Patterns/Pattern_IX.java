eabe43710ea02132cf4091e4ce60ebe5b24455be
Dushyant Goyal <dushyantgoyal1999@gmail.com>
2021-06-22 21:09:15 +0530

    5th Commit

--- src/Patterns/Pattern_IX.java
+++ src/Patterns/Pattern_IX.java
@@ -1,23 +1,23 @@
 /*
-        *
-      *   *
-    *   *   *
-  *   *   *   *
-*   *   *   *   *
+* * * * *
+    * * * *
+        * * *
+            * *
+                *
 
-*/
+ */
 package Patterns;
 import java.util.*;
-public class Pattern_VII {
+public class Pattern_IX {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=n-i; j++){
+        for(int i = 1; i<=n; i++) {
+            for (int j = 1; j <= i + i; j++) {
                 System.out.print("  ");
             }
-            for (int j = 1; j<=i; j++){
-                System.out.print("*   ");
+            for (int k = 1; k <= n - i + 1; k++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
---
+++ src/Patterns/Pattern_VIII.java
@@ -0,0 +1,27 @@
+/*
+*
+* *
+*    *
+*      *
+* * * * *
+
+*/
+package Patterns;
+import java.util.*;
+public class Pattern_VIII {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("*");
+        for(int j = 2; j<=n-1; j++) {
+            System.out.print("* ");
+            for (int k = 1; k <= j - 2; k++) {
+                System.out.print("  ");
+            }
+            System.out.println("* ");
+        }
+        for(int i =1; i<=n; i++){
+            System.out.print("* ");
+        }
+    }
+}
